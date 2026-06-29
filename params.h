3a6a553e8d61004ac639b5842a8d7e4f0ed4cf6d
runqing xu <xu_runqing@foxmail.com>
2026-06-29 09:24:22 +0800

    params: make all six TRINE parameter sets build-selectable

--- params.h
+++ params.h
@@ -3,50 +3,122 @@
 
 #include <stdint.h>
 
+#ifndef PARAMS
 #define PARAMS 1
+#endif
+
+#if PARAMS < 1 || PARAMS > 6
+#error "PARAMS must be in the range 1..6"
+#endif
 
 #if PARAMS == 1
+#define TRINE_PARAMETER_SET_NAME "Balanced-I"
+#define TRINE_PARAMETER_TARGET_SUFFIX "balanced_i"
 #define TRINE_lambda 128
 #define TRINE_n 22
 #define TRINE_q 4093
 #define TRINE_r 138
 #define TRINE_K 52
 #define TRINE_X 1
+#define TRINE_EXPECTED_VECTOR_BYTES 33u
+#define TRINE_EXPECTED_MATRIX_BYTES 726u
+#define TRINE_EXPECTED_TRIFORM_BYTES 15972u
+#define TRINE_EXPECTED_PK_BYTES 16004u
+#define TRINE_EXPECTED_SK_BYTES 32u
+#define TRINE_EXPECTED_RESPONSE_BYTES 1716u
+#define TRINE_EXPECTED_BASE_SEED_COUNT 86u
+#define TRINE_EXPECTED_BASE_SEED_BYTES 1376u
+#define TRINE_EXPECTED_SIG_BYTES 3156u
 #elif PARAMS == 2
+#define TRINE_PARAMETER_SET_NAME "Balanced-III"
+#define TRINE_PARAMETER_TARGET_SUFFIX "balanced_iii"
 #define TRINE_lambda 256
 #define TRINE_n 40
 #define TRINE_q 4093
 #define TRINE_r 271
 #define TRINE_K 104
 #define TRINE_X 1
+#define TRINE_EXPECTED_VECTOR_BYTES 60u
+#define TRINE_EXPECTED_MATRIX_BYTES 2400u
+#define TRINE_EXPECTED_TRIFORM_BYTES 96000u
+#define TRINE_EXPECTED_PK_BYTES 96064u
+#define TRINE_EXPECTED_SK_BYTES 64u
+#define TRINE_EXPECTED_RESPONSE_BYTES 6240u
+#define TRINE_EXPECTED_BASE_SEED_COUNT 167u
+#define TRINE_EXPECTED_BASE_SEED_BYTES 5344u
+#define TRINE_EXPECTED_SIG_BYTES 11712u
 #elif PARAMS == 3
+#define TRINE_PARAMETER_SET_NAME "Balanced-V"
+#define TRINE_PARAMETER_TARGET_SUFFIX "balanced_v"
 #define TRINE_lambda 512
 #define TRINE_n 81
 #define TRINE_q 4093
 #define TRINE_r 534
 #define TRINE_K 211
 #define TRINE_X 1
+#define TRINE_EXPECTED_VECTOR_BYTES 122u
+#define TRINE_EXPECTED_MATRIX_BYTES 9842u
+#define TRINE_EXPECTED_TRIFORM_BYTES 797162u
+#define TRINE_EXPECTED_PK_BYTES 797290u
+#define TRINE_EXPECTED_SK_BYTES 128u
+#define TRINE_EXPECTED_RESPONSE_BYTES 25637u
+#define TRINE_EXPECTED_BASE_SEED_COUNT 323u
+#define TRINE_EXPECTED_BASE_SEED_BYTES 20672u
+#define TRINE_EXPECTED_SIG_BYTES 46565u
 #elif PARAMS == 4
+#define TRINE_PARAMETER_SET_NAME "ShortSig-I"
+#define TRINE_PARAMETER_TARGET_SUFFIX "shortsig_i"
 #define TRINE_lambda 128
 #define TRINE_n 22
 #define TRINE_q 4093
 #define TRINE_r 61
 #define TRINE_K 36
 #define TRINE_X 4
+#define TRINE_EXPECTED_VECTOR_BYTES 33u
+#define TRINE_EXPECTED_MATRIX_BYTES 726u
+#define TRINE_EXPECTED_TRIFORM_BYTES 15972u
+#define TRINE_EXPECTED_PK_BYTES 63920u
+#define TRINE_EXPECTED_SK_BYTES 32u
+#define TRINE_EXPECTED_RESPONSE_BYTES 1188u
+#define TRINE_EXPECTED_BASE_SEED_COUNT 25u
+#define TRINE_EXPECTED_BASE_SEED_BYTES 400u
+#define TRINE_EXPECTED_SIG_BYTES 1652u
 #elif PARAMS == 5
+#define TRINE_PARAMETER_SET_NAME "ShortSig-III"
+#define TRINE_PARAMETER_TARGET_SUFFIX "shortsig_iii"
 #define TRINE_lambda 256
 #define TRINE_n 40
 #define TRINE_q 4093
 #define TRINE_r 116
 #define TRINE_K 76
 #define TRINE_X 4
+#define TRINE_EXPECTED_VECTOR_BYTES 60u
+#define TRINE_EXPECTED_MATRIX_BYTES 2400u
+#define TRINE_EXPECTED_TRIFORM_BYTES 96000u
+#define TRINE_EXPECTED_PK_BYTES 384064u
+#define TRINE_EXPECTED_SK_BYTES 64u
+#define TRINE_EXPECTED_RESPONSE_BYTES 4560u
+#define TRINE_EXPECTED_BASE_SEED_COUNT 40u
+#define TRINE_EXPECTED_BASE_SEED_BYTES 1280u
+#define TRINE_EXPECTED_SIG_BYTES 5968u
 #elif PARAMS == 6
+#define TRINE_PARAMETER_SET_NAME "ShortSig-V"
+#define TRINE_PARAMETER_TARGET_SUFFIX "shortsig_v"
 #define TRINE_lambda 512
 #define TRINE_n 81
 #define TRINE_q 4093
 #define TRINE_r 232
 #define TRINE_K 149
 #define TRINE_X 4
+#define TRINE_EXPECTED_VECTOR_BYTES 122u
+#define TRINE_EXPECTED_MATRIX_BYTES 9842u
+#define TRINE_EXPECTED_TRIFORM_BYTES 797162u
+#define TRINE_EXPECTED_PK_BYTES 3188776u
+#define TRINE_EXPECTED_SK_BYTES 128u
+#define TRINE_EXPECTED_RESPONSE_BYTES 18104u
+#define TRINE_EXPECTED_BASE_SEED_COUNT 83u
+#define TRINE_EXPECTED_BASE_SEED_BYTES 5312u
+#define TRINE_EXPECTED_SIG_BYTES 23672u
 #endif
 
 
@@ -83,6 +155,15 @@ _Static_assert(
     TRINE_q <= ((uint32_t)1u << TRINE_q_bits),
     "TRINE_q_bits is too small");
 
+_Static_assert(
+    (uint32_t)UINT16_MAX >= (TRINE_q - 1u),
+    "Fq cannot store q - 1");
+
+_Static_assert(
+    (uint64_t)TRINE_n * (uint64_t)(TRINE_q - 1u) * (uint64_t)(TRINE_q - 1u)
+        <= UINT64_MAX,
+    "matrix dot-product accumulator may overflow");
+
 #define TRINE_CEIL_DIV(x,y) (((x) + (y) - 1) / (y))
 
 #define TRINE_lambda_bytes (TRINE_lambda / 8)
@@ -106,13 +187,14 @@ _Static_assert(
 #define TRINE_SALT_OFFSET (TRINE_DIGEST_OFFSET + TRINE_digest_bytes)
 #define TRINE_SIG_BYTES (TRINE_SALT_OFFSET + TRINE_salt_bytes)
 
-_Static_assert(TRINE_VECTOR_BYTES == 33, "unexpected vector size");
-_Static_assert(TRINE_TRIFORM_BYTES == 15972, "unexpected triform size");
-_Static_assert(TRINE_PK_BYTES == 16004, "unexpected public-key size");
-_Static_assert(TRINE_SK_BYTES == 32, "unexpected secret-key size");
-_Static_assert(TRINE_RESPONSE_BYTES == 1716, "unexpected response size");
-_Static_assert(TRINE_BASE_SEED_COUNT == 86, "unexpected base-seed count");
-_Static_assert(TRINE_BASE_SEED_BYTES == 1376, "unexpected base-seed segment size");
-_Static_assert(TRINE_SIG_BYTES == 3156, "unexpected signature size");
+_Static_assert(TRINE_VECTOR_BYTES == TRINE_EXPECTED_VECTOR_BYTES, "unexpected vector size");
+_Static_assert(TRINE_MATRIX_BYTES == TRINE_EXPECTED_MATRIX_BYTES, "unexpected matrix size");
+_Static_assert(TRINE_TRIFORM_BYTES == TRINE_EXPECTED_TRIFORM_BYTES, "unexpected triform size");
+_Static_assert(TRINE_PK_BYTES == TRINE_EXPECTED_PK_BYTES, "unexpected public-key size");
+_Static_assert(TRINE_SK_BYTES == TRINE_EXPECTED_SK_BYTES, "unexpected secret-key size");
+_Static_assert(TRINE_RESPONSE_BYTES == TRINE_EXPECTED_RESPONSE_BYTES, "unexpected response size");
+_Static_assert(TRINE_BASE_SEED_COUNT == TRINE_EXPECTED_BASE_SEED_COUNT, "unexpected base-seed count");
+_Static_assert(TRINE_BASE_SEED_BYTES == TRINE_EXPECTED_BASE_SEED_BYTES, "unexpected base-seed segment size");
+_Static_assert(TRINE_SIG_BYTES == TRINE_EXPECTED_SIG_BYTES, "unexpected signature size");
 
 #endif
